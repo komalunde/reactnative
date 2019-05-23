c3fc217bb6857cfd7798eb96e31042a3bda38afa
komalunde <undekomal@gmail.com>
2019-05-23 15:06:54 +0530

    data Manage using Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+import Root from './Root';
 
 export const load = () => {
-}
+  ReactDOM.render(
+      <Root />,
+    document.getElementById('demo1')
+  );
+};
 
 try {
-  ////load()
-} catch(e) {}
+  load();
+} catch (e) {}
