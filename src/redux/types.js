8b66d139b1b4963a0495c427d8f4783e26eaae31
komalunde <undekomal@gmail.com>
2019-05-27 17:11:37 +0530

    Deployment

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,6 +1,6 @@
 export const FETCH_NEW_TIME = 'FETCH_NEW_TIME';
 export const LOGIN = 'USER_LOGIN';
-//export const LOGOUT = 'USER_LOGOUT';
+export const LOGOUT = 'USER_LOGOUT';
 export const TRY_LOGIN = 'AUTH_TRY_LOGIN';
 export const LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'AUTH_LOGIN_FAILURE';
